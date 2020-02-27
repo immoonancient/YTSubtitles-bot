e766839e6a4f6e061e39d1a4a61d4e5579baf86e
Nora <30676044+NoraZheng@users.noreply.github.com>
2020-02-26 22:07:35 -0500

    Update formatter/formatter.js
    Co-Authored-By: Xiaocheng Hu <51099011+immoonancient@users.noreply.github.com>

--- formatter/formatter.js
+++ formatter/formatter.js
@@ -1,6 +1,8 @@
 const Timeline = require('./timeline.js');
 
-const REGEX_CHINESE = /[\u4e00-\u9fff]|[\u3400-\u4dbf]|[\u{20000}-\u{2a6df}]|[\u{2a700}-\u{2b73f}]|[\u{2b740}-\u{2b81f}]|[\u{2b820}-\u{2ceaf}]|[\uf900-\ufaff]|[\u3300-\u33ff]|[\ufe30-\ufe4f]|[\uf900-\ufaff]|[\u{2f800}-\u{2fa1f}]/u;
+// Table 12-2, Blocks Containing Han Ideographs
+// http://www.unicode.org/versions/Unicode5.0.0/ch12.pdf
+const REGEX_CHINESE = /[\u4e00-\u9fff]|[\u3400-\u4dbf]|[\u{20000}-\u{2a6df}]|[\u{2a700}-\u{2b73f}]|[\u{2b740}-\u{2b81f}]|[\u{2b820}-\u{2ceaf}]|[\uf900-\ufaff]|[\u{2f800}-\u{2fa1f}]/u;
 
 function hasChinese(str) {
   return REGEX_CHINESE.test(str);
